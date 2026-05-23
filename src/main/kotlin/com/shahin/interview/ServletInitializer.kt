0b3fe25774b91fca757569ed2860b16d971527c5
shahin325-admin <shahin.safari1369@gmail.com>
2026-05-23 11:34:19 +0300

    add files

---
+++ src/main/kotlin/com/shahin/interview/ServletInitializer.kt
@@ -0,0 +1,12 @@
+package com.shahin.interview
+
+import org.springframework.boot.builder.SpringApplicationBuilder
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer
+
+class ServletInitializer : SpringBootServletInitializer() {
+
+    override fun configure(application: SpringApplicationBuilder): SpringApplicationBuilder {
+        return application.sources(InterviewApplication::class.java)
+    }
+
+}
---
+++ src/test/kotlin/com/shahin/interview/InterviewApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.shahin.interview
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class InterviewApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
